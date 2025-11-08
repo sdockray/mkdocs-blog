5045557499d87beb708c18925ba04c74949f9b05
sdockray <sean.patrick.dockray@gmail.com>
2025-11-08 18:21:03 +1100

    settings for blog

--- preview.sh
+++ preview.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Change these lines
 mkdocs_path="/Users/sdoc0001/.pyenv/shims/mkdocs"
-config_path="/Users/sdoc0001/Documents/dev/prj/obsidian-utils/mkdocs-spd/mkdocs.yml"
+config_path="/Users/sdoc0001/Documents/dev/prj/obsidian-utils/mkdocs-blog/mkdocs.yml"
 site_dir="/tmp/mkdocs-build"
 # Leave this alone
 eval "$mkdocs_path build --config-file $config_path --site-dir $site_dir"
